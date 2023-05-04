9b2a8305f009e6154f582d7294a03fdea0fe9303
Frankie Eder <frankaeder@gmail.com>
2023-05-03 20:59:51 -0700

    Basic starter.

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import plotly.graph_objects as go
+
+if __name__ == '__main__':
+    st.markdown("This tool helps determine the actual cost of a home purchase in California, USA,"
+                "given my understanding of tax and property laws currently standing. This is by no means"
+                "a guarantee of what you will end up paying for a home purchase, though please feel"
+                "free to email me at frankaeder@gmail.com with any corrections.")
