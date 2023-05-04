692f2e5595212bd6173528c177b9fa4f3506853c
Frankie Eder <frankaeder@gmail.com>
2023-05-03 22:28:27 -0700

    oopify.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,55 +12,85 @@ class LoanStatus:
     interest: float
     principal: float
 
-def calculate_mortgage_per_year(
-        loan_amount_usd: float,
-        interest_rate_percentage: float,
-        mortgage_years: float,
-):
-    numerator = (1 + interest_rate_percentage) ** mortgage_years
-    denomenator = numerator - 1
-    result = loan_amount_usd
-    result *= interest_rate_percentage
-    result *= numerator
-    result /= denomenator
-    return result
+@dataclass
+class PropertyLoan:
+    MONTHS_PER_YEAR = 12
 
+    loan_amount_usd: float
+    interest_rate_percentage: float
+    mortgage_years: float
 
-def calculate_monthly_interest(
-        balance_usd: float,
-        interest_rate_percentage: float
-):
-    return (balance_usd * interest_rate_percentage) / 12
+    @property
+    def mortgage_per_year_usd(self):
+        numerator = (1 + self.interest_rate_percentage) ** self.mortgage_years
+        denomenator = numerator - 1
+        result = self.loan_amount_usd
+        result *= self.interest_rate_percentage
+        result *= numerator
+        result /= denomenator
+        return result
 
+    @property
+    def mortgage_per_month_usd(self):
+        return self.mortgage_per_year_usd / self.MONTHS_PER_YEAR
 
-def calculate_monthly_principal(
-        mortgage_per_month_usd: float,
-        interest_usd: float,
-        balance_usd: float
-):
-    return (mortgage_per_month_usd - interest_usd) * (balance_usd > 0)
+    def calculate_monthly_interest(
+            self,
+            balance_usd: float,
+    ):
+        return (balance_usd * self.interest_rate_percentage) / 12
 
+    def calculate_monthly_principal(
+            self,
+            interest_usd: float,
+            balance_usd: float
+    ):
+        return (self.mortgage_per_month_usd - interest_usd) * (balance_usd > 0)
 
-def calculate_next_loan_status(
-        loan_status: LoanStatus
-) -> LoanStatus:
-    next_months_balance = max(loan_status.balance - loan_status.principal, 0)
-    next_months_interest = calculate_monthly_interest(
-        balance_usd=next_months_balance,
-        interest_rate_percentage=interest_rate_percentage
-    )
-    next_months_principal = calculate_monthly_principal(
-        mortgage_per_month_usd=mortgage_per_month_usd,
-        interest_usd=next_months_interest,
-        balance_usd=next_months_balance
-    )
-    next_loan_status = LoanStatus(
-        balance=next_months_balance,
-        interest=next_months_interest,
-        principal=next_months_principal
-    )
-    return next_loan_status
+    def calculate_next_loan_status(
+            self,
+            loan_status: LoanStatus
+    ) -> LoanStatus:
+        next_months_balance = max(loan_status.balance - loan_status.principal, 0)
+        next_months_interest = self.calculate_monthly_interest(
+            balance_usd=next_months_balance
+        )
+        next_months_principal = self.calculate_monthly_principal(
+            interest_usd=next_months_interest,
+            balance_usd=next_months_balance
+        )
+        next_loan_status = LoanStatus(
+            balance=next_months_balance,
+            interest=next_months_interest,
+            principal=next_months_principal
+        )
+        return next_loan_status
 
+    @property
+    def initial_loan_status(self) -> LoanStatus:
+        first_months_interest = self.calculate_monthly_interest(
+            balance_usd=self.loan_amount_usd,
+        )
+        first_months_principal = self.calculate_monthly_principal(
+            interest_usd=first_months_interest,
+            balance_usd=self.loan_amount_usd
+        )
+        initial_loan_status = LoanStatus(
+            balance=self.loan_amount_usd,
+            interest=first_months_interest,
+            principal=first_months_principal,
+        )
+        return initial_loan_status
+
+    def __iter__(self):
+        loan_status = self.initial_loan_status
+        yield loan_status
+        while loan_status.balance > 0:
+            loan_status = self.calculate_next_loan_status(loan_status)
+            yield loan_status
+
+    def as_dataframe(self):
+        return pd.DataFrame(iter(self))
 
 
 if __name__ == '__main__':
@@ -109,37 +139,14 @@ if __name__ == '__main__':
     )
 
     loan_amount_usd = purchase_price * (1 - down_payment_percentage)
-    st.markdown(f"Loan Amount: {loan_amount_usd}")
 
-    mortgage_per_year_usd = calculate_mortgage_per_year(
+    property_loan = PropertyLoan(
         loan_amount_usd=loan_amount_usd,
         interest_rate_percentage=interest_rate_percentage,
         mortgage_years=mortgage_years,
     )
-    mortgage_per_month_usd = mortgage_per_year_usd / MONTHS_PER_YEAR
+    st.markdown(f"Loan Amount: {property_loan.loan_amount_usd}")
 
-    st.write(mortgage_per_year_usd)
-    first_months_interest = calculate_monthly_interest(
-        balance_usd=loan_amount_usd,
-        interest_rate_percentage=interest_rate_percentage
-    )
-    first_months_principal = calculate_monthly_principal(
-        mortgage_per_month_usd=mortgage_per_month_usd,
-        interest_usd=first_months_interest,
-        balance_usd=loan_amount_usd
-    )
-
-    loan_statuses = [
-        LoanStatus(
-            balance=loan_amount_usd,
-            interest=first_months_interest,
-            principal=first_months_principal,
-        )
-    ]
-
-    while loan_statuses[-1].balance > 0:
-        loan_statuses.append(calculate_next_loan_status(loan_statuses[-1]))
-
-    loan_statuses = pd.DataFrame(loan_statuses)
+    loan_statuses = property_loan.as_dataframe()
 
     st.write(loan_statuses)
